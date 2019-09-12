1b8df2ad3169a34c94c6487d6d95d53564a3cce4
Liz Laffitte <liz.kossakowski@gmail.com>
2019-09-12 14:02:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -71,7 +71,9 @@ def runner
   card_total = initial_round
   p "Up to #{card_total}"
   until card_total > 21
+    p "Up to #{card_total}"   
     hit?(card_total)
+    p "Up to #{card_total}"
     display_card_total(card_total)
   end 
   end_game(card_total)
